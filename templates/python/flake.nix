44b03fce03850025edc6be974253a7d632bcf491
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-04-15 21:52:52 -0500

    feat(python): new template python

---
+++ templates/python/flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "A Python Proyect Template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    eachSystems = f:
+      lib.genAttrs systems
+      (system: let
+        pkgs = import nixpkgs {inherit system;};
+        pypi = pkgs.python3.withPackages (pypi:
+          with pypi; [
+            #add your python libraries here
+          ]);
+      in
+        f {
+          inherit pkgs;
+          system = "${system}";
+          nativeBuildInputs = with pkgs; [
+            python3
+            #add you packages
+          ];
+          buildInputs = [
+            # add Your packages and libraries
+          ];
+          packages = [
+            pypi
+          ];
+        });
+  in {
+    devShells = eachSystems ({
+      pkgs,
+      nativeBuildInputs,
+      buildInputs,
+      packages,
+      ...
+    }: {
+      default = pkgs.mkShell {
+        inherit nativeBuildInputs buildInputs packages;
+      };
+    });
+    formatter = eachSystems ({pkgs, ...}: pkgs.alejandra);
+  };
+}

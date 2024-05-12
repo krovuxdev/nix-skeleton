c3503c3ad7641eb9b28043d6f14ba9124fbb5ca9
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-05-11 22:48:17 -0500

    Feat: Update flake.nix for add template bevyengine

--- flake.nix
+++ flake.nix
@@ -2,18 +2,24 @@
   description = "Start Nix projects quickly with this simple template";
   outputs = { self }: {
     templates = {
-        rust = {
-            path = ./templates/rust;
-            description = "A Rust proyect template";
-          };
-        python = {
-            path = ./templates/python;
-            description = "A python proyect template";
-          };
+      rust = {
+        path = ./templates/rust/default;
+        description = "A Rust proyect template";
+        bevy = {
+          path = ./templates/rust/engine/bevy;
+          description = "A Rust proyect template for bevyengine";
+        };
       };
+      python = {
+        path = ./templates/python;
+        description = "A python proyect template";
+      };
+    };
     defaultTemplate = self.templates.rust;
     #alias
     rs = self.templates.rust;
+    "rust#bevy" = self.templates.rust.bevy;
+    "rs#bevy" = self.templates.rust.bevy;
     py = self.templates.python;
   };
 }
